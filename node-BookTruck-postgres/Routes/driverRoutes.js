0f65a62167f09143b2a820e29fccdcfaf588891a
Altharia <althariam@gmail.com>
2023-06-03 13:55:37 +0200

    add

--- node-BookTruck-postgres/Routes/driverRoutes.js
+++ node-BookTruck-postgres/Routes/driverRoutes.js
@@ -3,3 +3,28 @@ const router = express.Router()
 const authController = require('../controllers/driverController')
 const controller = require('../controllers/driverController')
 const verify = require('../middleware/verifyJWT')
+
+
+router.route('/')
+    .get(productController.getAllProducts)
+    .post(productController.createNewProduct)
+
+router.route('/:id')
+    .get(productController.getProductById)
+
+    
+router.route('/:id')
+
+    .delete(productController.deleteprod)
+
+
+    router.route('/:id')
+    .put(productController.updateProd)
+
+
+
+
+
+//router.route('/:id').delete(productController.delete)    
+
+module.exports = router
--- node-BookTruck-postgres/index.js
+++ node-BookTruck-postgres/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const db = require('./queries')
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -17,4 +19,14 @@ app.get('/', (request, response) => {
     console.log(`App running on port ${port}.`)
   })
 
+  app.get('/drivers', db.getUsers)
+  app.get('/drivers/:id', db.getUserById)
+  app.post('/drivers', db.createUser)
+  app.put('/drivers/:id', db.updateUser)
+  app.delete('/drivers/:id', db.deleteUser)
+  
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+  
   
